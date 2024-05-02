de2e1d544264b4bef7f2bc0bc10b6ad955ced43c
praveensharma5749 <133798691+praveensharma5749@users.noreply.github.com>
2024-05-02 10:07:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue() {
     //Write your code here
-    var form = document.querySelector("#form1");
+	var form = document.querySelector("#form1");
     let first = document.querySelector("[name='fname']").value;
     let second = document.querySelector("[name='lname']").value;
     
