aa2f4b0b3fb4fe074bdb4a9beae51d8bfd5da28b
Praveen <waytosearch454@gmail.com>
2024-05-02 10:04:16 +0530

    Code Edited

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ function getFormvalue() {
     var form = document.querySelector("#form1");
     let first = document.querySelector("[name='fname']").value;
     let second = document.querySelector("[name='lname']").value;
-    console.log(first);
-    console.log(second);
+    
     alert(
         first + " " + second
     )
