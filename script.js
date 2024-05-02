dd9f626b6d49a81f2360cab12359de2354aae5b9
Praveen <waytosearch454@gmail.com>
2024-05-02 10:03:25 +0530

    Alert Code Added

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
-
+    var form = document.querySelector("#form1");
+    let first = document.querySelector("[name='fname']").value;
+    let second = document.querySelector("[name='lname']").value;
+    console.log(first);
+    console.log(second);
+    alert(
+        first + " " + second
+    )
 }
